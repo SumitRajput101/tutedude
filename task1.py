d9b5690c338b9087a7485a3f5a49f3ca333d27c4
SumitRajput101 <kumarsumeet540@gmail.com>
2025-06-24 12:55:05 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,7 @@
+####### TASK ! ########
+a = int(input("Enter no 1 :"))
+b = int(input("enter no 2 :"))
+print("Addtion:", a+b)
+print("subraction:", a-b)
+print("Multiplication:", a*b)
+print("Divison:", a/b)
---
+++ task2.py
@@ -0,0 +1,3 @@
+fname = input("Enter your first name : ")
+lname = input("enter your last name : ")
+print(f"Hello, {fname} {lname}! Welcome to the python program")
